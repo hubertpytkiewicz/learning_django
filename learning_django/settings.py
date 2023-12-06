0da6a3822b8d8191b599f8227442179126faf6bd
hubert <hubertpytkiewicz2@gmail.com>
2023-12-06 22:25:48 +0100

    Organize static files and fix minor bugs

--- learning_django/settings.py
+++ learning_django/settings.py
@@ -120,6 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# For later usage
+# STATIC_ROOT = []
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
